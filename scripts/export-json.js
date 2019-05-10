05fa42704db843178b3c7c6ab845f544fb6e628e
solstice-sebastian <solstice.sebastian@gmail.com>
2019-05-09 20:08:55 -0700

    export json script

---
+++ scripts/export-json.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb');
+const { writeFileSync } = require('fs');
+const dotenv = require('dotenv');
+const { resolve } = require('path');
+
+dotenv.config();
+
+const { MONGO_URL } = process.env;
+const collName = 'all_results';
+const dbName = 'cadence_simulator';
+const outputPath = resolve(__dirname, '..', 'data', `${collName}.json`);
+
+const getDb = async () => {
+  const client = await MongoClient.connect(
+    MONGO_URL,
+    { useNewUrlParser: true }
+  );
+  return client.db(dbName);
+};
+
+const run = async () => {
+  const db = await getDb();
+  const coll = await db.collection(collName);
+  const docs = await coll.find({}).toArray();
+  const jsonData = JSON.stringify(docs, null, 2);
+  writeFileSync(outputPath, jsonData, 'utf8');
+  process.exit(1);
+};
+
+run();
